0243acc52dde4294c162925577a9f4d6f209892a
Minor Tom <i7245143@w32317.student.bournemouth.ac.uk>
2016-01-27 14:23:35 +0000

    Adding nvcc system call support
    Former-commit-id: 57b6d253c0cbcc5d243a935980fcc3e2af2ecd67
    Former-commit-id: 5b444b6793cf5362b899cb56c9ab8779f348c01c
    Former-commit-id: 4636b4f5c3fde4dc83fda77a2d0620a51a2dffe9

--- Romanesco_Src/src/runtimecompiler.cpp
+++ Romanesco_Src/src/runtimecompiler.cpp
@@ -12,14 +12,14 @@ RuntimeCompiler::RuntimeCompiler(const std::string &_name, const std::string _so
                                  std::vector<std::string> _includeFiles)
     : m_result(nullptr)
 {
-#ifdef NVRTC_AVAILABLE
-    NVRTC_SAFE_CALL( nvrtcCreateProgram(&m_prog, _source.c_str(), _name.c_str(), 0, NULL, NULL) );
-
     std::vector<const char*> opts;
     opts.push_back("--gpu-architecture=compute_35");
     opts.push_back("-rdc=true");
-    opts.push_back("-I/home/tom/src/Romanesco/QtTest/kernel");
+    opts.push_back("-I$HOME/src/Romanesco/QtTest/kernel");
     opts.push_back("-I/usr/local/cuda-7.0/targets/x86_64-linux/include");
+
+#ifdef NVRTC_AVAILABLE
+    NVRTC_SAFE_CALL( nvrtcCreateProgram(&m_prog, _source.c_str(), _name.c_str(), 0, NULL, NULL) );
 
     nvrtcResult compileResult = nvrtcCompileProgram(m_prog, opts.size(), opts.data());
 
@@ -44,7 +44,11 @@ RuntimeCompiler::RuntimeCompiler(const std::string &_name, const std::string _so
     m_result = new char[ptxSize];
     NVRTC_SAFE_CALL(nvrtcGetPTX(m_prog, m_result));
 #else
-    qDebug() << MACROTOSTRING(CUDA_EXE);
+    const std::string nvccbin = MACROTOSTRING(CUDA_EXE);
+    const std::string nvccflags= "--compiler-options -fno-strict-aliasing -use_fast_math --ptxas-options=-v -ptx";
+    const std::string nvcccall = nvccbin + " -m64 -gencode arch=compute_50,code=sm_50 " + nvccflags;
+
+    system( nvcccall.c_str() );
 
     m_result = new char[strlen(_source.c_str())];
     strcpy(m_result, _source.c_str());
