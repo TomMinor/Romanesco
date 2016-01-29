b40e988a5cc865ea2a6f9613a8331df2d92564cc
Tom Minor <i7245143@bournemouth.ac.uk>
2016-01-29 10:53:46 +0000

    latest
    Former-commit-id: 260de99afb6772f3b466703591d999b5dfac9d6c
    Former-commit-id: eb2ca735efc64fb28a1755960550f53932ebadf1
    Former-commit-id: b938ea1dcb75a84081c5fb41883de0fa334e4309

--- Romanesco_Src/src/gui/nodes/distanceopnode.cpp
+++ Romanesco_Src/src/gui/nodes/distanceopnode.cpp
@@ -1,10 +1,13 @@
 #include "include/gui/nodes/distanceopnode.h"
 #include "qneport.h"
 
+static int ctr = 0;
+
 DistanceOpNode::DistanceOpNode(const QString &_name, QGraphicsScene* _scene, QGraphicsItem *parent)
     : QNEBlock(_scene, parent)
 {
-    addPort(_name, 0, QNEPort::NamePort);
+    QString name = QString("%1_%2").arg(_name).arg(ctr++);
+    addPort(name, 0, QNEPort::NamePort);
     addPort("Distance", 0, QNEPort::TypePort);
 
     addInputPort("A");
