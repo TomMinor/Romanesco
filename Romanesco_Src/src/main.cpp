d1213cf3c22864b660c50457391e07910b00f269
Minor Tom <i7245143@w32309.student.bournemouth.ac.uk>
2016-04-10 20:47:13 +0100

    latest
    Former-commit-id: e8340a13f985fc0c6a2d7d76a6e0c78621c5e9af
    Former-commit-id: c05896c2d7655f59edeaf3ec2244af02257f3580
    Former-commit-id: e6919e8f27bc557dcdb07ae673945188b2b92712

--- Romanesco_Src/src/main.cpp
+++ Romanesco_Src/src/main.cpp
@@ -3,6 +3,8 @@
 //#include <SDL.h>
 //#include <SDL_haptic.h>
 
+#include <SeExpression.h>
+
 #include <QSurfaceFormat>
 //#include <shaderwindow.h>
 
@@ -12,8 +14,116 @@
 #include <vector>
 #include <QDebug>
 
+
+#include <OpenImageIO/imageio.h>
+
+
+using namespace OpenImageIO;
+
+
+
+
+
+class GrapherExpr:public SeExpression
+{
+public:
+    //! Constructor that takes the expression to parse
+    GrapherExpr(const std::string& expr)
+        :SeExpression(expr)
+    {}
+
+    //! set the independent variable
+    void setX(double x_input)
+    {x.val=x_input;}
+
+private:
+    //! Simple variable that just returns its internal value
+    struct SimpleVar:public SeExprScalarVarRef
+    {
+        double val; // independent variable
+        void eval(const SeExprVarNode* /*node*/,SeVec3d& result)
+        {result[0]=val;}
+    };
+
+    //! independent variable
+    mutable SimpleVar x;
+
+    //! resolve function that only supports one external variable 'x'
+    SeExprVarRef* resolveVar(const std::string& name) const
+    {
+        if(name == "x") return &x;
+        return 0;
+    }
+};
+
+
+
+
+
 int main(int argc, char *argv[])
 {
+    std::string exprStr="\
+                         $val=.5*PI*x;\
+                         7*sin(val)/val \
+                         ";
+//    GrapherExpr expr(exprStr);
+
+//    double xmin=-10,xmax=10,ymin=-10,ymax=10;
+//    int w=60,h=30;
+//    char *buffer=new char[w*h];
+//    memset(buffer,(int)' ',w*h);
+
+//    // draw x axis
+//    int j_zero=(-ymin)/(ymax-ymin)*h;
+//    if(j_zero>=0 && j_zero<h){
+//        for(int i=0;i<w;i++){
+//            buffer[i+j_zero*w]='-';
+//        }
+//    }
+//    // draw y axis
+//    int i_zero=(-xmin)/(xmax-xmin)*w;
+//    if(i_zero>=0 && i_zero<w){
+//        for(int j=0;j<h;j++){
+//            buffer[i_zero+j*w]='|';
+//        }
+//    }
+
+//    // evaluate the graph
+//    const int samplesPerPixel=10;
+//    const double one_over_samples_per_pixel=1./samplesPerPixel;
+//    for(int i=0;i<w;i++)
+//    {
+//        for(int sample=0;sample<samplesPerPixel;sample++)
+//        {
+//            // transform from device to logical coordinatex
+//            double dx=double(sample)*one_over_samples_per_pixel;
+//            double x=double(dx+i)/double(w)*(xmax-xmin)+xmin;
+//            // prep the expression engine for evaluation
+//            expr.setX(x);
+//            // evaluate and pull scalar value
+//            SeVec3d val=expr.evaluate();
+//            double y=val[0];
+//            // transform from logical to device coordinate
+//            int j=(y-ymin)/(ymax-ymin)*h;
+//            // store to the buffer
+//            if(j>=0 && j<h)
+//                buffer[i+j*w]='#';
+//        }
+//    }
+
+//    // draw the graph from the buffer
+//    for(int j=h-1;j>=0;j--){
+//        for(int i=0;i<w;i++){
+//            std::cout<<buffer[i+j*w];
+//        }
+//        std::cout<<std::endl;
+//    }
+
+
+
+
+
+
 //  if (SDL_Init(SDL_INIT_JOYSTICK | SDL_INIT_HAPTIC) < 0 )
 //  {
 //    // Or die on error
