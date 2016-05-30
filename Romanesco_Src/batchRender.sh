ab26688d503e18e1ab4708f6e02af1c6f93277f6
Tom Minor <i7245143@bournemouth.ac.uk>
2016-05-30 19:35:07 +0100

    fix render res override
    Former-commit-id: 559f69a25905e6ef652efb633bca087bdead6140 [formerly 16511df419a5d7876669b5ce971160ab3da7cd85] [formerly b56132aff70259ac43270ee02884e65296c84cd2 [formerly cd8df35f697998a61fedc08f8c0ceb0a4e0e9aa5]]
    Former-commit-id: ebd1307d94a4571c5ea60942344da1876fb49d23 [formerly 4f9b5061ce74459eb80fa213fff13d065210881b]
    Former-commit-id: 735278131990a0eae9ac4d0018b200fbca3eb89d
    Former-commit-id: 1722d768ff2269b56a61ac66ac8b3d8be0850e78
    Former-commit-id: ea2ff87168bb2820e38ef60f1723e1e56a0fb46d

--- Romanesco_Src/batchRender.sh
+++ Romanesco_Src/batchRender.sh
@@ -8,33 +8,33 @@ mkdir -p $OUTPUTPATH
 WIDTH=1920
 HEIGHT=1080
 
-# spc_sh_070 f115 @todo fly towards camera
-SHOTFOLDER=$OUTPUTPATH/spc_sh_070; mkdir -p $SHOTFOLDER;
-./romanesco $GLOBALARGS -s 0 -e 115 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/spc_sh_070.cu --timeout 20
+# spc_sh_070 f115 start:2294 @todo fly towards camera
+#SHOTFOLDER=$OUTPUTPATH/spc_sh_070; mkdir -p $SHOTFOLDER;
+#./romanesco $GLOBALARGS -s 0 -e 115 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/spc_sh_070.cu --timeout 20
 
 # fra_sh_010 f152 @todo Fix the mandelbulb
 # ./romanesco -s 0 -e 10 -f /transfer/fractals/out_%04d.exr -a 1920 -b 1080 -i ./kernel/fra_sh_010.cu --timeout 20
 
-# fra_sh_020 f106
+# fra_sh_020 f106 start:2561
 SHOTFOLDER=$OUTPUTPATH/fra_sh_020; mkdir -p $SHOTFOLDER;
 ./romanesco $GLOBALARGS -s 0 -e 106 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/fra_sh_020.cu --timeout 20
 
-# fra_sh_030 f90
+# fra_sh_030 f90 start:2667
 SHOTFOLDER=$OUTPUTPATH/fra_sh_030; mkdir -p $SHOTFOLDER;
 ./romanesco $GLOBALARGS -s 107 -e 197 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/fra_sh_030.cu --timeout 20
 
-# fra_sh_040 f69
+# fra_sh_040 f69 start:2757
 SHOTFOLDER=$OUTPUTPATH/fra_sh_040; mkdir -p $SHOTFOLDER;
 ./romanesco $GLOBALARGS -s 198 -e 267 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/fra_sh_040.cu --timeout 20
 
-# fra_sh_050 f78
+# fra_sh_050 f78 start:2826
 SHOTFOLDER=$OUTPUTPATH/fra_sh_050; mkdir -p $SHOTFOLDER;
 ./romanesco $GLOBALARGS -s 268 -e 346 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/fra_sh_050.cu --timeout 20
 
-# fra_sh_060 f92
+# fra_sh_060 f92 start:2904
 SHOTFOLDER=$OUTPUTPATH/fra_sh_060; mkdir -p $SHOTFOLDER;
 ./romanesco $GLOBALARGS -s 347 -e 439 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/fra_sh_060.cu --timeout 20
 
-# fra_sh_070 f61
+# fra_sh_070 f61 start:2996
 SHOTFOLDER=$OUTPUTPATH/fra_sh_070; mkdir -p $SHOTFOLDER;
 ./romanesco $GLOBALARGS -s 440 -e 501 -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./kernel/fra_sh_070.cu --timeout 20
