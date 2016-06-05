cafac0280cf82da8468285abe73232cdb329098b
Tom Minor <i7245143@bournemouth.ac.uk>
2016-06-05 16:34:35 +0100

    new iteration for sideshot
    Former-commit-id: 52419a92e4afd6999e00b190b94f0ed67ef7b779 [formerly 9ddd606acc73fbdb71a20de2c5b0f3cfa2ce74f6] [formerly 2a3b9cc2734898d428200642350ae4f292643bc2 [formerly b860b14f80bde24f292f8bd673c2028eeb061eb4]]
    Former-commit-id: bf808801440d9bdbf19cd84380099b492e31ed23 [formerly 9911c6460982095b77d25d546f3b847bf5fcc26a]
    Former-commit-id: 9602216b1b7b10e512f3f3c82bc179f5fdd221a8
    Former-commit-id: 3747935e0406e395a0520d49ae4a252972a97e7d
    Former-commit-id: ef1d1f6f7c5d70fed4361569b797ac1d169777e4

--- Romanesco_Src/scenes/fra_sh_010.cu
+++ Romanesco_Src/scenes/fra_sh_010.cu
@@ -6,7 +6,7 @@
 
 HIT_PROGRAM float2 hit(float3 x, int maxIterations, float global_t)
 {
-	x.z += (global_t / 25.0f);
+	x.z += (global_t / 35.0f) * 0.8;
 	Matrix4x4 transform = Matrix4x4::rotate( global_t / 25.0f, make_float3(0,0,1) );
 	x = applyTransform(x, transform);
 
@@ -14,8 +14,8 @@ HIT_PROGRAM float2 hit(float3 x, int maxIterations, float global_t)
 	sdf.setTime(global_t);
 	sdf.evalParameters();
 	
-	float oscillatingTime = sin( global_t / 300.0f );
-	float p = (5.0f * oscillatingTime) + 3.0f;
+	float oscillatingTime = sin( (global_t / 90.0f) * 0.8 );
+	float p = (2.0f * oscillatingTime) + 5.0f;
 	sdf.setPower(p);
 
 	return make_float2( sdf.evalDistance(x), sdf.getTrap() );
