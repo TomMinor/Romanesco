3eb9cae77facd2d9630eff879573ed55b149fc23
Minor Tom <i7245143@w32309.student.bournemouth.ac.uk>
2016-06-07 00:07:37 +0100

    Fixed EXR alignment issues
    Former-commit-id: c6a20adac3075d9d5e840dda91806c3f56d9c26a [formerly 29f91a2c0ca836a04bd4063bcd195d06151d829c] [formerly befd3904dae066105024bb9af99b86b670fb8fcc [formerly 535c4e076f717cd92754dd919dc00543d6168229]]
    Former-commit-id: 4e54f5afbb5dba2c1056e84ebd46cd6127eef31e [formerly fb81405887652ddd32d8bb93f5c58252b2ed4694]
    Former-commit-id: 98306e5f7d1e592b50a5aded04eed1b0b49671e1
    Former-commit-id: ae6ab8a397091ae788a7ad120feddb7ee207fbee
    Former-commit-id: a19d9a0a3a03f1b9cfca0abe65f33be63c1020f6
    Former-commit-id: 396dd997e0239b4b297f9dafbd01a7993c2c78c5
    Former-commit-id: a8967aadb8c425c6fff5bc544b61d4e67bd0f07b

--- Romanesco_Src/scenes/default.cu
+++ Romanesco_Src/scenes/default.cu
@@ -6,11 +6,11 @@
 
 HIT_PROGRAM float2 hit(float3 x, int maxIterations, float global_t)
 {
-	Mandelbulb sdf(maxIterations);
-	sdf.evalParameters();
-    	sdf.setTime(global_t);
-	float p = (5.0 * abs(sin(global_t / 40.0))) + 3.0;
-	sdf.setPower( p );
+    MengerSponge sdf(maxIterations);
+    sdf.evalParameters();
+    sdf.setTime(global_t);
+//	float p = (5.0 * abs(sin(global_t / 40.0))) + 3.0;
+//	sdf.setPower( p );
 
 	return make_float2( sdf.evalDistance(x), sdf.getTrap() );
 }
