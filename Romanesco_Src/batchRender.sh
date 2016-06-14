336775d0a200444144a8138b1c352035ad2e4332
Minor Tom <i7245143@w32310.student.bournemouth.ac.uk>
2016-06-14 01:42:39 +0100

    the one I used to render everything
    Former-commit-id: 8c4ce3c1dd4de6eaa89159643ac9c29d60d10d71 [formerly f23f2c11f6296641c5497e6ac73c4f2627af6195] [formerly 921c090eed7451989737ca5983ae31b121eb57b2 [formerly 158f90e8750e024dea4b4b817a050ade53898ac1]]
    Former-commit-id: 4058ee825c2ae21ed662cab169cbc25a42e28233 [formerly e4cc170b30c81db17a84cf4ab3fb1e9fd2c40507]
    Former-commit-id: 3b2a945ae0553fcf665017c6e4d042ff5400f830
    Former-commit-id: 9b40d66e6881914eb75383714edadae0b4279a36
    Former-commit-id: 493a9b13ed548a33cbe1c3d02a0cc850e82566a0

--- Romanesco_Src/batchRender.sh
+++ Romanesco_Src/batchRender.sh
@@ -27,8 +27,8 @@ TILEY=16
 # Associative array for easy shot data editing
 declare -A SHOTS
 # ================== Core Shots ======================
-SHOTS[spc_sh_070]="0 	115 0" #2294  115
-SHOTS[fra_sh_010]="0 	152 0" #2409  152
+#SHOTS[spc_sh_070]="0 	115 0" #2294  115
+#SHOTS[fra_sh_010]="0 	152 0" #2409  152
 SHOTS[fra_sh_020]="0 	106 0" #2561  106
 SHOTS[fra_sh_030]="120 	210 0" #2667  90
 SHOTS[fra_sh_040]="198	267 0" #2757  69
@@ -37,38 +37,50 @@ SHOTS[fra_sh_060]="347 	439 0" #2904  92
 SHOTS[fra_sh_070]="440 	501 0" #2996  61
 
 # ================== Fix Up Shots ======================
-SHOTS[fra_sh_010_a]="0 	30 0 fra_sh_010"
-SHOTS[fra_sh_010_b]="30 60 30 fra_sh_010"
-SHOTS[fra_sh_010_c]="60 90 60 fra_sh_010"
-SHOTS[fra_sh_010_d]="90 120 90 fra_sh_010"
-SHOTS[fra_sh_010_e]="120 152 120 fra_sh_010"
+#SHOTS[fra_sh_010_a]="0 	30 0 fra_sh_010"
+#SHOTS[fra_sh_010_b]="30 60 30 fra_sh_010"
+#SHOTS[fra_sh_010_c]="60 90 60 fra_sh_010"
+#SHOTS[fra_sh_010_d]="90 120 90 fra_sh_010"
+#SHOTS[fra_sh_010_e]="120 152 120 fra_sh_010"
 
-SHOTS[fra_sh_020_a]="0 0 44 fra_sh_020"
+#SHOTS[fra_sh_020_a]="0 0 44 fra_sh_020"
 
-SHOTS[fra_sh_030_a]="0 0 56 fra_sh_030"
+#SHOTS[fra_sh_030_b]="56 56 -1 fra_sh_030"
 
-SHOTS[fra_sh_040_a]="13 28 -1 fra_sh_040"
-SHOTS[fra_sh_040_b]="0 0 36 fra_sh_040"
+SHOTS[fra_sh_040_a]="0 11 -1 fra_sh_040" #17
+SHOTS[fra_sh_040_b]="12 22 -1 fra_sh_040" #17 
+SHOTS[fra_sh_040_c]="23 34 -1 fra_sh_040" #17
+SHOTS[fra_sh_040_d]="35 45 -1 fra_sh_040" #17
+SHOTS[fra_sh_040_e]="46 56 -1 fra_sh_040" #17
+SHOTS[fra_sh_040_f]="57 69 -1 fra_sh_040" #17
 
-SHOTS[fra_sh_050_a]="0 0 9 fra_sh_050"
-SHOTS[fra_sh_050_b]="0 0 18 fra_sh_050"
 
-SHOTS[fra_sh_060_a]="0 0 70 fra_sh_060"
-SHOTS[fra_sh_060_b]="0 0 76 fra_sh_060"
+#SHOTS[fra_sh_050_a]="0 0 9 fra_sh_050"
+#SHOTS[fra_sh_050_b]="0 0 18 fra_sh_050"
 
-SHOTS[fra_sh_070_a]="0 0 32 fra_sh_070"
-SHOTS[fra_sh_070_b]="0 0 41 fra_sh_070"
+#SHOTS[fra_sh_060_a]="0 0 70 fra_sh_060"
+#SHOTS[fra_sh_060_b]="0 0 76 fra_sh_060"
+
+#SHOTS[fra_sh_070_a]="0 0 32 fra_sh_070"
+#SHOTS[fra_sh_070_b]="0 0 41 fra_sh_070"
 
 # Shots  broken up into pieces
-SHOTS[spc_sh_070_a]="0 	 28 0 spc_sh_070" # 152
-SHOTS[spc_sh_070_b]="28  58 28 spc_sh_070"
-SHOTS[spc_sh_070_c]="58  86 58 spc_sh_070"
-SHOTS[spc_sh_070_d]="86 115 86 spc_sh_070"
+#SHOTS[spc_sh_070_a]="0 	 28 0 spc_sh_070" # 152
+#SHOTS[spc_sh_070_b]="28  58 28 spc_sh_070"
+#SHOTS[spc_sh_070_c]="58  86 58 spc_sh_070"
+#SHOTS[spc_sh_070_d]="86 115 86 spc_sh_070"
 
 # Extra frames for wiggle room
-SHOTS[spc_sh_070_e]="116 130 116 spc_sh_070"
-SHOTS[spc_sh_070_f]="131 150 130 spc_sh_070"
-SHOTS[spc_sh_070_g]="150 180 150 spc_sh_070"
+#SHOTS[spc_sh_070_e]="116 130 116 spc_sh_070"
+#SHOTS[spc_sh_070_f]="131 150 130 spc_sh_070"
+#SHOTS[spc_sh_070_g]="150 180 150 spc_sh_070"
+
+SHOTS[fra_sh_070_ext_a]="62 66 -1 fra_sh_070"
+SHOTS[fra_sh_070_ext_b]="67 70 -1 fra_sh_070"
+SHOTS[fra_sh_070_ext_c]="71 74 -1 fra_sh_070"
+SHOTS[fra_sh_070_ext_d]="75 79 -1 fra_sh_070"
+SHOTS[fra_sh_070_ext_e]="80 84 -1 fra_sh_070"
+SHOTS[fra_sh_070_ext_f]="85 90 -1 fra_sh_070"
 
 
 
@@ -157,12 +169,12 @@ do
 	endframe="${framedata[1]}"
 	frameoffset="${framedata[2]}"
 
-	if [ "$OVERRIDEFRAMES" = true ]; then
-		echo "$(tput setaf $error_color)Overriding frame range to $START:$END$(tput sgr0)"
-		endframe=$END
-		startframe=$START
-        frameoffset=$START
-	fi
+	# if [ "$OVERRIDEFRAMES" = true ]; then
+	# 	echo "$(tput setaf $error_color)Overriding frame range to $START:$END$(tput sgr0)"
+	# 	endframe=$END
+	# 	startframe=$START
+ #        frameoffset=$START
+	# fi
 
 	if [[ -z ${FINAL+x} ]];
 	then
@@ -181,70 +193,3 @@ do
         echo
 done
 
-
-echo ".. Removing duplicate environment shot inputs..."
-sorted_unique_ids=($(echo "${@}" | tr ' ' '\n' | sort -u | tr '\n' ' '))
-
-printf ".. Environment Shots to process: $(tput setaf $highlight_color) ${sorted_unique_ids[*]} $(tput sgr0)";
-tput sgr0
-echo
-
-echo "-------------------------------------------------"
-echo ".. Removing invalid environment shot inputs..."
-for shot in "${sorted_unique_ids[@]}"
-do
-	if array_contains "$shot" "${!ENVTEXTURES[@]}"; then
-		: # Shot is valid
-	else
-		delete=($shot)
-		sorted_unique_ids=("${sorted_unique_ids[@]/$delete}")
-
-		echo "$(tput setaf $error_color) Warning: Environment shot $shot doesn't exist, skipping $(tput sgr0)" 
-	fi
-done
-echo "-------------------------------------------------"
-echo ".. Final list of environment shots to process..."
-printf ".. Shots to process: "; printf "$(tput setaf $highlight_color) ${sorted_unique_ids[*]} $(tput sgr0)\n";
-echo
-
-final_shots=(${sorted_unique_ids[@]})
-for shot in "${final_shots[@]}"
-do
-	framedata=(${ENVTEXTURES[$shot]})
-
-	shotname=$shot
-	parentshotname=${framedata[3]}
-
-	# Is the parent shot defined?
-	if [[ -n  "$parentshotname" ]]
-	then
-		shotname="$parentshotname"
-	fi
-
-	startframe="${framedata[0]}"
-	endframe="${framedata[1]}"
-	frameoffset="${framedata[2]}"
-
-	if [ "$OVERRIDEFRAMES" = true ]; then
-		echo "$(tput setaf $error_color)Overriding frame range to $START:$END$(tput sgr0)"
-		endframe=$END
-		startframe=$START
-        frameoffset=$START
-	fi
-
-	if [[ -z ${FINAL+x} ]];
-	then
-		SHOTFOLDER=$OUTPUTPATH/$shotname/images/sphericalFractals_hd; mkdir -p $SHOTFOLDER;
-
-		printf "Starting final quality environment map render for shot $(tput setaf $highlight_color)$shotname$(tput sgr0)... Start: $(tput setaf $frame_color)[%s]$(tput sgr0)\tEnd: $(tput setaf $frame_color)[%s]$(tput sgr0)\tOffset: $(tput setaf $frame_color)[%s]$(tput sgr0)\n" $startframe $endframe $frameoffset
-		printf "Output directory: $(tput setaf $frame_color)%s$(tput sgr0)" $SHOTFOLDER
-		$EXECUTABLE $GLOBALARGS $FINALARGS --environmentCamera -s $startframe -e $endframe --tileX $TILEX --tileY $TILEY--offset $frameoffset -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./scenes/$shotname.cu
-	else
-		SHOTFOLDER=$OUTPUTPATH/$shotname/images/sphericalFractals; mkdir -p $SHOTFOLDER;
-
-		printf "Starting environment map render for shot $(tput setaf $highlight_color)$shotname$(tput sgr0)... Start: $(tput setaf $frame_color)[%s]$(tput sgr0)\tEnd: $(tput setaf $frame_color)[%s]$(tput sgr0)\tOffset: $(tput setaf $frame_color)[%s]$(tput sgr0)\n" $startframe $endframe $frameoffset
-		printf "Output directory: $(tput setaf $frame_color)%s$(tput sgr0)" $SHOTFOLDER
-		$EXECUTABLE $GLOBALARGS $PREVIEWARGS --environmentCamera -s $startframe -e $endframe --tileX $TILEX --tileY $TILEY --offset $frameoffset -f $SHOTFOLDER/$OUTPUTFILENAME --width $WIDTH --height $HEIGHT -i ./scenes/$shotname.cu
-	fi
-	echo
-done
---
+++ Romanesco_Src/quickrender.sh
@@ -0,0 +1,2 @@
+OUTPUTPATH=$KYRANPC
+./romanesco -b --timeout 1 -s 56 -e 56 --tileX 16 --tileY 16 -f $OUTPUTPATH/fra_sh_030/images/fractals/fractal.%04d.exr --width 1920 --height 1080 -i ./scenes/fra_sh_030.cu
