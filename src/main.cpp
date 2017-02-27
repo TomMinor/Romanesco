a88057a59051d7b4b5c67fbb5809a719b7fe03b9
Tom Minor <tornronim@gmail.com>
2017-02-27 00:59:28 +0000

    CMake now detects cuda,optix,qt and I've started making this work on windows. Might need to remove boost dependencies because it's only being used for a join. Also need to setup OpenEXR/OpenImageIO detection
    Former-commit-id: 66df7dfd7d2a5ce5accc701262306d11d8221a3a [formerly 7e563af0c9ac3eaff80fb35a418f4753fd2efa9d] [formerly 0bcb1964bd40fdd7227f89cd8ee2b541789694cc [formerly 49503c534765d2ddce568ed440ec54a1c0604c01]]
    Former-commit-id: ff58fe480b8e40cccb1fc5feeeb572bcdbb93a84 [formerly 374549abf290a5e62442238b6e7b9ca8d6ec21e6]
    Former-commit-id: fb6e1be016d77e3460c116bb71992e29b2929d33
    Former-commit-id: 803397bf269e94dcb8d9ed9baf91dacaaa13c486
    Former-commit-id: fca62a463ab57546ccd278ff57fac48de293573d
    Former-commit-id: 2760f74d1eb99b588e3d0d999b64b62c0bbc73ca
    Former-commit-id: d878227aa3d4596e21fedb6318df7760c67c4223

--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,57 @@
+
+
 #include <QApplication>
-//#include <SDL.h>
-//#include <SDL_haptic.h>
-
-#include <SeExpression.h>
-
+////#include <SDL.h>
+////#include <SDL_haptic.h>
+//
+//#include <SeExpression.h>
+//
 #include <QSurfaceFormat>
-//#include <shaderwindow.h>
+////#include <shaderwindow.h>
 
 #include <iostream>
 #include <vector>
 #include <QDebug>
 
-#include "mainwindow.h"
+//#include "mainwindow.h"
 
-#include "qframebuffer.h"
+//#include "qframebuffer.h"
 
 #include <future>
 #include <iostream>
 
-
-
 int main(int argc, char *argv[])
 {
-  QApplication app(argc, argv);
-  QCoreApplication::setApplicationName("Romanesco Renderer");
-  QCoreApplication::setApplicationVersion("1.0");
+//  QApplication app(argc, argv);
+//  QCoreApplication::setApplicationName("Romanesco Renderer");
+//  QCoreApplication::setApplicationVersion("1.0");
+//
+//
+//
+//  QSurfaceFormat format;
+//  //format.setVersion(4, 3);
+//  format.setProfile(QSurfaceFormat::CoreProfile);
+//  format.setDepthBufferSize(24);
+//  format.setStencilBufferSize(8);
+//  QSurfaceFormat::setDefaultFormat(format);
+//
+//  MainWindow window;
+//  //window.setFormat(format);
+//
+//  // Load and set stylesheet
+//  QFile file("styles/romanesco.qss");
+//  file.open(QFile::ReadOnly);
+//  QString stylesheet = QLatin1String(file.readAll());
+//  window.setGlobalStyleSheet(stylesheet);
+//
+//  window.setWindowTitle("Romanesco");
+//  window.resize(1280, 800);
+//  window.show();
+//
+////  window.setAnimating(true);
+//
+//
+//  return app.exec();
 
-
-
-  QSurfaceFormat format;
-  //format.setVersion(4, 3);
-  format.setProfile(QSurfaceFormat::CoreProfile);
-  format.setDepthBufferSize(24);
-  format.setStencilBufferSize(8);
-  QSurfaceFormat::setDefaultFormat(format);
-
-  MainWindow window;
-  //window.setFormat(format);
-
-  // Load and set stylesheet
-  QFile file("styles/romanesco.qss");
-  file.open(QFile::ReadOnly);
-  QString stylesheet = QLatin1String(file.readAll());
-  window.setGlobalStyleSheet(stylesheet);
-
-  window.setWindowTitle("Romanesco");
-  window.resize(1280, 800);
-  window.show();
-
-//  window.setAnimating(true);
-
-
-  return app.exec();
+	return 0;
 }
