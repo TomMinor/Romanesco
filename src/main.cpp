7d2bec7d94e67e865eb5c02bce107f0a5d53a1bc
Tom Minor <tom.minor@thefoundry.co.uk>
2017-04-06 13:40:28 +0100

    Cleaned up main

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,5 @@
-
-
 #include <QApplication>
-////#include <SDL.h>
-////#include <SDL_haptic.h>
-//
-//#include <SeExpression.h>
-
 #include <QSurfaceFormat>
-////#include <shaderwindow.h>
-
-//#include <OpenImageIO\imageio.h>
 
 #include <iostream>
 #include <vector>
@@ -21,18 +11,15 @@
 #include <future>
 #include <iostream>
 
+////#include <SDL.h>
+////#include <SDL_haptic.h>
+//#include <SeExpression.h>
 
 /* TODO
- * Fix nvrtc on linux
- * FIx optix crash
  * Rename TestGLWidget to something better
- * Move image code back to OpenEXR
  * Make all resource paths relative (to what)
- * Add nvcc building to cmake
- * Fix texture issue with viewport
  * Why is the viewport so slow (profile?)
  * Cleanup sutil dependency
- * Cleanup OpenEXR dependency
  * Clearly comment nvidia code (PinholeCamera)
  */
 
@@ -53,7 +40,6 @@ int main(int argc, char *argv[])
 	QSurfaceFormat::setDefaultFormat(format);
 
 	MainWindow window;
-	//window.setFormat(format);
 
 	// Load and set stylesheet
 	QFile file("styles/romanesco.qss");
@@ -64,11 +50,6 @@ int main(int argc, char *argv[])
 	window.setWindowTitle("Romanesco");
 	window.resize(1280, 800);
 	window.show();
-
-	optix::Context a = optix::Context::create();
-
-
-	//  window.setAnimating(true);
 
 	app.exec();
 }
